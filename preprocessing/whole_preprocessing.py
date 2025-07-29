93352fc5cce48a929e3880e29a059bfb3abc2680
Yan Genev <yanthepro1234@gmail.com>
2025-07-29 14:47:15 +0300

    added whole_processing.py

--- preprocessing/whole_preprocessing.py
+++ preprocessing/whole_preprocessing.py
@@ -1,11 +1,15 @@
 from noise_filter import apply_correction
 
 print("Starting preprocessing...")
+#      \/ add land segmentation here \/
 
+
+
+#    this is the noise filter
 image_path = "ts.png"
 for i in range(1, 2):
     enhanced_image = apply_correction(image_path)
     image_path ="test_output.png"
 
-#      \/ add land segmentation here \/
+
 
