ffd04072c658daecc8fc50c97b629841b4419b86
Yan Genev <yanthepro1234@gmail.com>
2025-07-30 14:55:51 +0300

    finished preprocessing

--- preprocessing/whole_preprocessing.py
+++ preprocessing/whole_preprocessing.py
@@ -1,21 +1,22 @@
 from noise_filter import apply_correction
 from Yan_segmentation import yan_mask
+from segment import process_image
 import cv2
 import numpy as np
 
-image_path = "vv2.jpg"
-image = cv2.imread(image_path)
-#      \/ add land segmentation here + remove yan_mask if needed \/
-
-image = yan_mask(image)
+image_path = "hi.png"
 
 print("Starting preprocessing...")
+#      \/ add land segmentation here \/
+
+image = process_image(image_path, visualize=True)
+
+
 
 for i in range(1, 2):
     enhanced_image = apply_correction(image)
     image_path ="test_output.png"
 
-cv2.imwrite("test_output.jpg", image)
 
 
 
